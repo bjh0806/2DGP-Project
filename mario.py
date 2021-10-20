6adfc9dc36c3991e0b581677eae7a6522f6efaf1
bjh0806 <miel0806@naver.com>
2021-10-20 14:55:20 +0900

    캐릭터 추가

--- mario.py
+++ mario.py
@@ -2,14 +2,23 @@ from pico2d import *
 
 WIDTH, HEIGHT = 1600, 900
 
-open_canvas(WIDTH, HEIGHT)
+open_canvas(WIDTH // 2, 600)
 
 sky = load_image('sky.png')
 ground = load_image('ground.png')
+mario = load_image('mario.png')
 
-sky.draw_now(WIDTH // 2, HEIGHT // 2)
-ground.draw_now(WIDTH // 2, HEIGHT // 2)
+x = 0
+frame = 0
 
-delay(5)
+while x < WIDTH:
+    clear_canvas()
+    sky.draw_now(WIDTH // 4, 200)
+    ground.draw_now(WIDTH // 4, 350)
+    mario.clip_draw(frame * 50, 0, 50, 50, x, 100)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
 
 close_canvas()
