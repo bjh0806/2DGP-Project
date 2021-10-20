717d9b5e39ac8d39b11cd393ba38d95918d76105
bjh0806 <miel0806@naver.com>
2021-10-20 14:58:27 +0900

    종료 키 구현

--- mario.py
+++ mario.py
@@ -8,15 +8,27 @@ sky = load_image('sky.png')
 ground = load_image('ground.png')
 mario = load_image('mario.png')
 
+running = True
 x = 0
 frame = 0
 
-while x < WIDTH:
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+while x < WIDTH and running:
     clear_canvas()
     sky.draw_now(WIDTH // 4, 200)
     ground.draw_now(WIDTH // 4, 350)
     mario.clip_draw(frame * 50, 0, 50, 50, x, 100)
     update_canvas()
+
+    handle_events()
     frame = (frame + 1) % 8
     x += 5
     delay(0.05)
