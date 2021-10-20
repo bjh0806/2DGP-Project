7ef159a041298e6dc79d4abf928274c3a1ce4a9f
bjh0806 <miel0806@naver.com>
2021-10-20 17:34:57 +0900

    점프 모션 추가

--- mario.py
+++ mario.py
@@ -4,7 +4,7 @@ WIDTH, HEIGHT = 1600, 900
 left, right = 0, 1
 
 def handle_events():
-    global running, x, dir, left, right, Wait
+    global running, x, dir, left, right, Wait, Jump
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -22,6 +22,9 @@ def handle_events():
                 Wait = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
+            elif event.key == SDLK_SPACE:
+                Jump = 1
+                Wait = 0
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_a:
                 dir += 1
@@ -37,31 +40,63 @@ ground = load_image('ground.png')
 mario = load_image('mario.png')
 start = load_image('start.png')
 wait = load_image('wait.png')
+jump = load_image('jump.png')
 
 running = True
 x = 0
+y = 100
 frame = 0
 dir = 0
 Start = 1
 firstframe = 0
 Wait = 0
 waitframe = 0
+Jump = 0
+i = 0
+jumpframe = 0
 
 while running:
     clear_canvas()
     sky.draw_now(WIDTH // 4, 200)
     ground.draw_now(WIDTH // 4, 350)
     if Start == 1:
-        start.clip_draw(firstframe * 50, 0, 50, 50, x, 100)
+        start.clip_draw(firstframe * 50, 0, 50, 50, x, y)
     elif Wait == 1:
         if right == 1:
-            wait.clip_draw(waitframe * 50, 50, 50, 50, x, 100)
+            wait.clip_draw(waitframe * 50, 50, 50, 50, x, y)
         elif left == 1:
-            wait.clip_draw(waitframe * 50, 0, 50, 50, x, 100)
+            wait.clip_draw(waitframe * 50, 0, 50, 50, x, y)
+    elif Jump == 1:
+        if i == 0:
+            if right == 1:
+                x1, y1 = x, y
+                x3, y3 = x + 150, y
+                x2, y2 = x + 75, y + 75
+            elif left == 1:
+                x1, y1 = x, y
+                x3, y3 = x - 150, y
+                x2, y2 = x - 75, y + 75
+
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * x1 + (-4 * t ** 2 + 4 * t) * x2 + (2 * t ** 2 - t) * x3
+        y = (2 * t ** 2 - 3 * t + 1) * y1 + (-4 * t ** 2 + 4 * t) * y2 + (2 * t ** 2 - t) * y3
+
+        i += 4
+
+        if right == 1:
+            jump.clip_draw((jumpframe + 5) * 50, 50, 50, 50, x, y)
+        elif left == 1:
+            jump.clip_draw((13 - jumpframe) * 50, 0, 50, 50, x, y)
+
+        if i == 104:
+            Jump = 0
+            i = 0
+
     elif right == 1:
-        mario.clip_draw(frame * 50, 50, 50, 50, x, 100)
+        mario.clip_draw(frame * 50, 50, 50, 50, x, y)
     elif left == 1:
-        mario.clip_draw((7 - frame) * 50, 0, 50, 50, x, 95)
+        mario.clip_draw((7 - frame) * 50, 0, 50, 50, x, y - 5)
+
     update_canvas()
 
     handle_events()
@@ -71,6 +106,8 @@ while running:
         x += 7
     elif Wait == 1:
         waitframe = (waitframe + 1) % 7
+    elif Jump == 1:
+        jumpframe = (jumpframe + 1) % 14
     else:
         frame = (frame + 1) % 8
         x += dir * 5
