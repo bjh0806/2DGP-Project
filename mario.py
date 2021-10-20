988fb1778cd3d2ac09cc8d3eb17123374326e3bc
bjh0806 <miel0806@naver.com>
2021-10-20 15:11:06 +0900

    캐릭터 좌우 이동

--- mario.py
+++ mario.py
@@ -2,6 +2,27 @@ from pico2d import *
 
 WIDTH, HEIGHT = 1600, 900
 
+def handle_events():
+    global running
+    global x
+    global dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_a:
+                dir -= 1
+            elif event.key == SDLK_d:
+                dir += 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_a:
+                dir += 1
+            elif event.key == SDLK_d:
+                dir -= 1
+
 open_canvas(WIDTH // 2, 600)
 
 sky = load_image('sky.png')
@@ -11,17 +32,9 @@ mario = load_image('mario.png')
 running = True
 x = 0
 frame = 0
+dir = 0
 
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-
-while x < WIDTH and running:
+while running:
     clear_canvas()
     sky.draw_now(WIDTH // 4, 200)
     ground.draw_now(WIDTH // 4, 350)
@@ -30,7 +43,9 @@ while x < WIDTH and running:
 
     handle_events()
     frame = (frame + 1) % 8
-    x += 5
+
+    x += dir * 5
+
     delay(0.05)
 
 close_canvas()
