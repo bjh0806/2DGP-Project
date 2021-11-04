ebedcb559313a552609d198a00117afa5aec3e49
bjh0806 <miel0806@naver.com>
2021-11-05 00:08:01 +0900

    타이틀 화면 추가

--- start_state.py
+++ start_state.py
@@ -1,3 +1,6 @@
+from pico2d import *
+import game_framework
+
 name = "StartState"
 image = None
 logo_time = 0.0
@@ -13,14 +16,18 @@ def exit():
 def update():
     global logo_time
 
-    if(logo_time > 1.0):
+    if logo_time > 1.0:
         logo_time = 0
         game_framework.quit()
+
     delay(0.01)
     logo_time += 0.01
 
 def draw():
     global image
     clear_canvas()
-    image.draw(800, 600)
+    image.draw(400, 300)
     update_canvas()
+
+def handle_events():
+    events = get_events()
