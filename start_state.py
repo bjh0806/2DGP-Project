e73fda23c1a355a242f6757bf0785c22690856a4
bjh0806 <miel0806@naver.com>
2021-11-06 23:29:49 +0900

    시작 화면 엔터 혹은 스페이스 누르면 넘어가도록 변경

--- start_state.py
+++ start_state.py
@@ -4,7 +4,6 @@ import mario
 
 name = "StartState"
 image = None
-logo_time = 0.0
 
 def enter():
     global image
@@ -14,21 +13,23 @@ def exit():
     global image
     del(image)
 
-def update():
-    global logo_time
-
-    if logo_time > 1.0:
-        logo_time = 0
-        game_framework.change_state(mario)
-
-    delay(0.01)
-    logo_time += 0.01
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ENTER):
+                game_framework.change_state(mario)
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(mario)
 
 def draw():
-    global image
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
 
-def handle_events():
-    events = get_events()
+def update():
+    pass
