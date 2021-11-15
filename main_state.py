d872db803518f585526da9b9830da898f8ff7c64
bjh0806 <miel0806@naver.com>
2021-11-16 00:18:29 +0900

    오브젝트 게임월드화

--- main_state.py
+++ main_state.py
@@ -3,21 +3,25 @@ import game_framework
 
 from sky import Sky
 from ground import Ground
+from object import Object
 
 name = "MainState"
 
 sky = None
 ground = None
+object = None
 
 def enter():
-    global sky, ground
+    global sky, ground, object
     sky = Sky()
     ground = Ground()
+    object = Object()
 
 def exit():
-    global sky, ground
+    global sky, ground, object
     del sky
     del ground
+    del object
 
 def pause():
     pass
@@ -33,9 +37,16 @@ def handle_events():
 def update():
     sky.update()
     ground.update()
+    object.update_random_box()
+    object.update()
+    delay(0.05)
     
 def draw():
     clear_canvas()
     sky.draw()
     ground.draw()
+    object.draw_random_box()
+    object.draw_upground()
+    object.draw_upground2()
+    object.draw_coin()
     update_canvas()
